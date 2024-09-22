3f61aba2a2303efedd7309bc9ea71db3fdcefb40
forittik <offi.rittik@gmail.com>
2024-09-22 13:49:28 +0530

    Requirements added

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Fire_Alert_Object_Detection_Project
+
+
+## How to run:
+
+```bash
+conda create -n FireDetection python=3.7 -y
+```
+
+```bash
+conda activate FireDetection
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'FireDetection',
+    version= '0.0.0',
+    author= 'Rittik_aka_mom',
+    author_email= 'offi.rittik@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
